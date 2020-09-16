8e3a7d9975440200a048fc1ae607a4686ca5abff
cw-jagrutee <69185686+cw-jagrutee@users.noreply.github.com>
2020-09-16 22:19:04 +0530

    Added AddPatient component

---
+++ client/src/Components/Patient/AddPatient.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const AddPatient = () => {
+  return (
+    <div>
+      <h3>Patient</h3>
+      <p>Patient Name</p>
+      <p>Age</p>
+      <p>Gender</p>
+      <p>Occupation</p>
+      <p>Address</p>
+      <p>Referred By</p>
+      <p>Past Dental / Medical History</p>
+      <p>Chief Complaint</p>
+      <p>O/E</p> 
+      <p>Provisional Diagnosis</p>
+      <p>Provisional Treatment Plan</p>
+    </div>
+  );
+}
+
+export default AddPatient;
